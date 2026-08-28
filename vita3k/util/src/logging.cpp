9b6f3df89a3c6e88a647aeaa99cc332663f01c72
zfitness <zfitcode@163.com>
2026-08-28 03:38:56 +0000

    feat: build config

--- vita3k/util/src/logging.cpp
+++ vita3k/util/src/logging.cpp
@@ -17,7 +17,6 @@
 
 #include <exception>
 #include <typeinfo>
-#include <util/fork_build.h>
 #include <util/log.h>
 
 #ifdef _WIN32
@@ -102,7 +101,6 @@ ExitCode init(const Root &root_paths, bool use_stdout) {
     if (add_sink(root_paths.get_log_path() / LOG_FILE_NAME) != Success)
         return InitConfigFailed;
     LOG_INFO("================= Vita3K session start =================");
-    LOG_INFO("FORK BUILD seq {} compiled {} {} | {}", FORK_BUILD_SEQ, __DATE__, __TIME__, FORK_BUILD_CHANGES);
 
     std::set_terminate([]() {
         if (const std::exception_ptr eptr = std::current_exception()) {
